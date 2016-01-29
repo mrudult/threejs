ac995dfc1200368908e1c382a57d36bd70fa05c7
Mrudul T <mrudultarwatkar@gmail.com>
2016-01-29 17:21:07 +0530

    final version added

--- gzip.sh
+++ gzip.sh
@@ -4,7 +4,5 @@ for file in css/*/*.css; do gzip -c "$file" > "$file.gz"; done
 for file in js/*.js; do gzip -c "$file" > "$file.gz"; done
 for file in js/*/*.js; do gzip -c "$file" > "$file.gz"; done
 for file in img/*.jpg; do gzip -c "$file" > "$file.gz"; done
-for file in images/*.jpg; do gzip -c "$file" > "$file.gz"; done
 for file in img/*.png; do gzip -c "$file" > "$file.gz"; done
-for file in images/*.png; do gzip -c "$file" > "$file.gz"; done
 for file in models/*/*.js; do gzip -c "$file" > "$file.gz"; done
